9cb19b23b8b23cfceb12f77f8f04fa8db1069f5d
antyaroslav <antoyaroslav@gmail.com>
2026-03-29 12:21:37 +0300

    add JUnit4 and bug report

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,40 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void shouldReturnAmountNeededToReachNextThousand() {
+    public void shouldReturnAmountNeededToReachNextThousand() {
         CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(300, service.remain(700));
+        Assert.assertEquals(800, service.remain(1200));
+        Assert.assertEquals(0, service.remain(1000));
+        Assert.assertEquals(300, service.remain(700));
+    }
 
-        assertEquals(300, service.remain(700));
-        assertEquals(800, service.remain(1200));
-        assertEquals(0, service.remain(1000));
-        assertEquals(300, service.remain(700));
+    @Test
+    public void shouldHandleZeroAmount() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void shouldHandleLargeAmounts() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(1000, service.remain(9999));
+    }
+
+    @Test
+    public void shouldReturnZeroWhenAmountIsExactlyThreshold() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldReturnCorrectRemainderForMultipleThresholds() {
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(0, service.remain(2000));
     }
 }
